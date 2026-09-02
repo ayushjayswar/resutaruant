79f36687b22a201d316e38e8eda9b186345dca33
Ayush Jayswar <ayushjayswra@gmail.com>
2026-09-02 13:52:27 +0530

    update contact button animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -218,12 +218,13 @@ const Contact = () => {
 
 
                                 {/* Send Button */}
+                               
                                 <button
-                                    type='submit'
-                                    className='font-semibold bg-red-600 hover:bg-red-700 text-white active:scale-95 px-6 py-2.5 rounded-full cursor-pointer transition duration-200'
+                                    class="relative px-8 py-2 rounded-md bg-white isolation-auto z-10 border-2 border-red-700 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full hover:text-white before:-right-full before:hover:right-0 before:rounded-full before:bg-[#A12347] before:-z-10 before:aspect-square before:hover:scale-150 overflow-hidden before:hover:duration-700 inline-flex items-center justify-center px-4 py-3 text-sm font-semibold text-black bg-white border border-gray-200 rounded-lg shadow-sm gap-x-2 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
                                 >
                                     Send
                                 </button>
+
 
                             </form>
 
